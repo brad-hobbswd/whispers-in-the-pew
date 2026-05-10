e8a51b59869ab4a53b799fd5641e8bbc0db25a30
Bradley Hobbs <brad.w.hobbs@gmail.com>
2026-05-09 21:26:11 -0400

    Fix shared footer path

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('partials/footer.html')
+fetch('/whispers-in-the-pew/partials/footer.html')
 .then(response => response.text())
 .then(data => {
 document.getElementById('footer').innerHTML = data;
